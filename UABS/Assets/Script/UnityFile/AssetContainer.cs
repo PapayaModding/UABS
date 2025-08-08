7a0a9006f43ded26f765c67304ed7ddd5fd07bf3
Kolyn090 <kolynlin@protonmail.com>
2025-08-08 11:01:22 -0400

    Asset container

---
+++ UABS/Assets/Script/UnityFile/AssetContainer.cs
@@ -0,0 +1,27 @@
+using AssetsTools.NET;
+using AssetsTools.NET.Extra;
+
+namespace UABS.Assets.Script.UnityFile
+{
+    // From UABEA
+    public class AssetContainer
+    {
+        public long PathId { get; }
+        public int ClassId { get; }
+        public ushort MonoId { get; }
+        public uint Size { get; }
+        public string FilePath { get; }
+        public AssetsFileInstance FileInstance { get; }
+#pragma warning disable CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
+        public AssetTypeValueField? BaseValueField { get; }
+#pragma warning restore CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
+
+        public long FilePosition { get; private set; }
+#pragma warning disable CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
+        public AssetsFileReader? FileReader { get; private set; }
+#pragma warning restore CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
+
+        public AssetPPtr AssetPPtr => new(FileInstance.path, 0, PathId);
+        public bool HasValueField => BaseValueField != null;
+    }
+}
