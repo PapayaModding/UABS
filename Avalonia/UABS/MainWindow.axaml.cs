5a42a658f5d508222a14023ad8c54162a54bdb99
Kolyn090 <kolynlin@protonmail.com>
2025-08-12 17:55:53 -0400

    Try Avalonia

---
+++ Avalonia/UABS/MainWindow.axaml.cs
@@ -0,0 +1,17 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace UABS;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        // Handle click
+    }
+}
