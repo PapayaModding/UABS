cb4647c6d02c11d22888b83016f443c2abd60b9c
Kolyn090 <kolynlin@protonmail.com>
2025-08-12 22:22:36 -0400

    Added a simple sort panel for now

---
+++ Avalonia/UABS/Script/View/Components/FolderViewSortPanel.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace UABS.Component;
+
+public partial class FolderViewSortPanel : UserControl
+{
+    public FolderViewSortPanel()
+    {
+        InitializeComponent();
+    }
+}
