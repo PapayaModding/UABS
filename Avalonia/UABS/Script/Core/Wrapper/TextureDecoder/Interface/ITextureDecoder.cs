d7280de7a8fccf54377a3d94f058bada31e0ae3f
Kolyn090 <kolynlin@protonmail.com>
2026-01-28 00:47:53 -0500

    Deleted old files

--- Avalonia/UABS/Script/Core/Wrapper/TextureDecoder/Interface/ITextureDecoder.cs
+++
@@ -1,8 +0,0 @@
-namespace UABS.Wrapper
-{
-    public interface ITextureDecoder
-    {
-        public IColor[] DecodeRaw(byte[] imageBytes, int width, int height, TextureCompressionFormat format);
-        byte[] DecodeToBytes(byte[] imageBytes, int width, int height, TextureCompressionFormat format);
-    }
-}
